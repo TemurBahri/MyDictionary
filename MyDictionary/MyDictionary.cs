d98ac631c431fdfa29f02121c249a3c2b63a7392
timurbahri@outlook.com <timurbahri@outlook.com>
2021-01-21 20:31:25 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<FValue>
+    {
+        FValue[] values;
+
+        public MyDictionary()
+        {
+
+            values = new FValue[0];
+        }
+
+        public void Add(FValue value)
+        {
+
+            FValue[] tempArray = values;
+
+            values = new FValue[values.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+
+                values[i] = tempArray[i];
+            }
+
+
+            values[values.Length - 1] = value;
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+
+            isimler.Add("Timur");
+
+
+            
+        }
+    }
+}
